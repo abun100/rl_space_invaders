9b6f7c2e1147479d02a1e722dde53a00bd9a2e1b
Aaron <abun02@uri.edu>
2023-11-16 09:59:44 -0500

    gameState.text

--- src/space_invaders/gameState_test.py
+++ src/space_invaders/gameState_test.py
@@ -9,8 +9,6 @@ class TestgameState(unittest.TestCase):
     def transform_image_test(self):
         start = env.reset()
         state_frames = gameState.State(start[0])
-        image = Image.fromarray(state_frames.obs[0])
-        image.save("original_state.png")
         
 if __name__ == '__main__':
     unittest.main()
